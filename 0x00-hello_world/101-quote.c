d275eb08de1316975ae6371a82a3af3d7b9455c1
nthabelengpeggy22 <nthabelengpeggy22@gmail.com>
2023-07-15 03:05:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unstd.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19.
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
