24c1f192fff3aa1763323723cf880e5eec5aa91c
nthabelengpeggy22 <nthabelengpeggy22@gmail.com>
2023-07-16 09:16:51 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-	int n;
+	int n = last digit
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", 32);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is negative\n", 53);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
