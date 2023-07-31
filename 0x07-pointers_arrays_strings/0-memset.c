5e563836f9d4e82ec94c1b0b73ee0a7ef79e2036
nthabelengpeggy22 <nthabelengpeggy22@gmail.com>
2023-07-31 10:37:01 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_memset -  fills the first n bytes of the memory area pointed
+ * _memset -  fills the first n bytes of the memory area pointed
  * to by s with the constant byte b
  * @s: memory area to be filled
  * @b: char to copy
