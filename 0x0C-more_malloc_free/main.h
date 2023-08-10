008b9fb57fdd6f6791586ac7f687fe5dc9a18553
nthabelengpeggy22 <nthabelengpeggy22@gmail.com>
2023-08-10 07:53:17 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
